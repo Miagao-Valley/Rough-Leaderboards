ed3416a551957a83c7d8b7fde5e5c59e1d0da6e2
Piage <piageaguirre@gmail.com>
2024-07-12 13:48:43 +0800

    Initial setup of mod

---
+++ src/main/java/mv/roughleaderboards/RoughLeaderboardsClient.java
@@ -0,0 +1,11 @@
+package mv.roughleaderboards;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RoughLeaderboardsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/mv/roughleaderboards/RoughLeaderboardsDataGenerator.java
@@ -0,0 +1,11 @@
+package mv.roughleaderboards;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class RoughLeaderboardsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
